bbd9caa09ca8c5db91e99a74d0838c5dbce8946e
thienthan1455 <thienthan1455@gmail.com>
2019-10-08 17:40:58 +0700

    update 8/10

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAlphabet();
+            PrintAlphabet2();
+            PrintAlphabetUP();
+            ASCII('A');
+            checkUpLow('b');
+
+        }
+        public static void PrintAlphabet()
+        {
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Console.Write(c);
+            }
+
+        }
+        public static void PrintAlphabet2()
+        {
+            for (char c = 'z'; c >= 'a'; c--)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        public static void PrintAlphabetUP()
+        {
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        public static void ASCII(char c)
+        {
+            Console.WriteLine((int)c);
+            for (int i = 65; i <= 122; i++)
+            {
+                if ((int)c == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public static void checkUpLow(char c)
+        {
+            for (int i = 65; i <= 90; i++)
+            {
+                if ((int)c == i)
+                {
+                    Console.WriteLine("{0} is UPPERCASE", c);
+                }
+            }
+
+
+            for (int j = 97; j <= 122; j++)
+            {
+                if ((int)c == j)
+                {
+                    Console.WriteLine("{0} is LOWCASE", c);
+                }
+            }
+
+        }
+
+    }
+}
+
+
